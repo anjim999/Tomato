ba95fbab6f2dfae4fac40ad10c53f3d43ce51d96
ALLUVENKATAREDDY <141143051+ALLUVENKATAREDDY@users.noreply.github.com>
2025-02-09 16:24:33 +0530

    Update ExploreMenu.jsx

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -6,7 +6,7 @@ const ExploreMenu = (props) => {
    const  {category,setCategory}=props;
   return (
     <div className='explore-menu' id='explore-menu'>
-        <h1>Exploreour menu</h1>
+        <h1>Explore our menu</h1>
         <p className='explore-menu-text'>Choose  from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience. one delicious meal at a time.</p>
         <div className="explore-menu-list">
             {
